a446fd17a63bb06ee590c18b18a30a018bbec40c
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-10-28 14:24:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ TOOLCHAIN=aarch64-linux-gnu-
 UBOOT_DIR=uboot-mtk-20250711
 #UBOOT_DIR=uboot-mtk-20230718-09eda825
 #ATF_DIR=atf-20220606-637ba581b
-ATF_DIR=atf-20250711
+ATF_DIR=atf-20240117-bacca82a8
 
 if [ -z "$SOC" ] || [ -z "$BOARD" ]; then
 	echo "Usage: SOC=[mt7981|mt7986] BOARD=<board name> MULTI_LAYOUT=[0|1] $0"
