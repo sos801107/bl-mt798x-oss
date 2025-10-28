2e467795318c253d18d33894924e655c53f4a7b3
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-10-28 20:35:55 +0800

    Update ubi_helper.c

--- uboot-mtk-20220606/board/mediatek/common/ubi_helper.c
+++ uboot-mtk-20220606/board/mediatek/common/ubi_helper.c
@@ -477,14 +477,30 @@ static int write_ubi_fit_image(const void *data, size_t size,
 	if (ret)
 		return ret;
 
-ifdef CONFIG_ENV_IS_IN_UBI
-	if (!find_ubi_volume(CONFIG_ENV_UBI_VOLUME))
-		create_ubi_volume(CONFIG_ENV_UBI_VOLUME， CONFIG_ENV_SIZE， -1， false);
-#endif
+	if (!find_ubi_volume("fit") && !find_ubi_volume("fip")) {
+		/* ubi is dirty, erase ubi and recreate volumes */
+		umount_ubi();
+		ubi_mtd_param_parse(mtd->name, NULL);
+		ret = mtd_erase_skip_bad(mtd, 0, mtd->size, mtd->size, NULL, NULL, false);
+		if (ret)
+			return ret;
+
+		ret = mount_ubi(mtd);
+		if (ret)
+			return ret;
+
+#ifdef CONFIG_ENV_IS_IN_UBI
+		ret = create_ubi_volume(CONFIG_ENV_UBI_VOLUME, CONFIG_ENV_SIZE, -1, false);
+		if (ret)
+			goto out;
 #ifdef CONFIG_SYS_REDUNDAND_ENVIRONMENT
-	if (!find_ubi_volume(CONFIG_ENV_UBI_VOLUME_REDUND))
-		create_ubi_volume(CONFIG_ENV_UBI_VOLUME_REDUND， CONFIG_ENV_SIZE， -1, false);
-#endif
+		ret = create_ubi_volume(CONFIG_ENV_UBI_VOLUME_REDUND, CONFIG_ENV_SIZE, -1, false);
+		if (ret)
+			goto out;
+#endif /* CONFIG_SYS_REDUNDAND_ENVIRONMENT */
+#endif /* CONFIG_ENV_IS_IN_UBI */
+	}
+
 	/* Remove this volume first in case of no enough PEBs */
 	remove_ubi_volume("rootfs_data");
 
